1f5b129ca67ece54d0d6326a8bf650969480dff7
Mike <dickinsonm87@yahoo.com>
2025-09-19 19:19:59 -0500

    change home page text

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "REST Countries - A React Router App" },
+    { name: "description", content: "REST Countries API with color theme switcher" },
   ];
 }
 
